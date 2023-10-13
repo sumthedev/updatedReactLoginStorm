7c96690d52454aae8dd09b8ec2ca657ab562301d
Sumbal Hashmi <115552450+sumthedev@users.noreply.github.com>
2023-10-13 23:31:37 +0500

    Update Logout.js

--- src/component/Logout.js
+++ src/component/Logout.js
@@ -3,11 +3,31 @@ import Navbar from "./Navbar";
 import { setName, setClick, setEmailValid } from "./redux/actions/authActions";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import MobileSize from "./mobileSize";
 
 export const Logout = (props) => {
   const rememberedEmail = JSON.parse(localStorage.getItem("rememberedEmail"));
   console.log(rememberedEmail);
   const navigate = useNavigate();
+  const [isSize, setMobileSize] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth <= 1064) {
+      setMobileSize(true);
+    } else {
+      setMobileSize(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     if (!rememberedEmail) {
@@ -25,57 +45,61 @@ export const Logout = (props) => {
   };
   return (
     <>
-      <div className="flex">
-        <div className="w-1/2 p-16 flex flex-col justify-center">
-          <img
-            src={process.env.PUBLIC_URL + "/logo.png"}
-            alt="Logo"
-            className=" relative w-[8%] mb-6 -mt-14"
-          />
+      {isSize === false ? (
+        <div className="flex">
+          <div className="w-1/2 p-16 flex flex-col justify-center">
+            <img
+              src={process.env.PUBLIC_URL + "/logo.png"}
+              alt="Logo"
+              className=" relative w-[8%] mb-6 -mt-14"
+            />
 
-          <div>
-            <h1 className="text-2xl font-semibold text-black mt-2 ">
-              <strong> Hello</strong>{" "}
-              <span className="text-gray-500">
-                <strong>
-                  {" "}
-                  {props.name ? props.name.split("@")[0] : "User"}{" "}
-                </strong>
-              </span>
-            </h1>
-            <p className="mt-4 text-black">You are logged in!</p>
+            <div>
+              <h1 className="text-2xl font-semibold text-black mt-2 ">
+                <strong> Hello</strong>{" "}
+                <span className="text-gray-500">
+                  <strong>
+                    {" "}
+                    {props.name ? props.name.split("@")[0] : "User"}{" "}
+                  </strong>
+                </span>
+              </h1>
+              <p className="mt-4 text-black">You are logged in!</p>
 
-            <button
-              onClick={handleLogout}
-              className="border  border-black mt-10 text-black py-2 px-4  ml-1 hover:bg-black hover:text-white transition duration-300"
-            >
-              Logout
-            </button>
+              <button
+                onClick={handleLogout}
+                className="border  border-black mt-10 text-black py-2 px-4  ml-1 hover:bg-black hover:text-white transition duration-300"
+              >
+                Logout
+              </button>
+            </div>
+
+            <p className="mt-10 text-black text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing
+            </p>
+            <p className=" font-semibold text-black text-sm">
+              libero omnis debitis quis quos ratione ullam
+            </p>
           </div>
 
-          <p className="mt-10 text-black text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing
-          </p>
-          <p className=" font-semibold text-black text-sm">
-            libero omnis debitis quis quos ratione ullam
-          </p>
-        </div>
-
-        <div
-          className="w-1/2 bg-cover bg-center h-screen"
-          style={{
-            backgroundImage: `url(${process.env.PUBLIC_URL}/flow2.jpg)`,
-          }}
-        >
-          <Navbar />
           <div
-            className="h-1/2 w-1/2 bg-black rounded-md bg-clip-padding backdrop-filter 
-          backdrop-blur-sm bg-opacity-20  relative mx-auto  mt-24 "
+            className="w-1/2 bg-cover bg-center h-screen"
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL}/flow2.jpg)`,
+            }}
           >
-            <img src={`${process.env.PUBLIC_URL}/m3.png`} alt="m" />
+            <Navbar />
+            <div
+              className="h-1/2 w-1/2 bg-black rounded-md bg-clip-padding backdrop-filter 
+          backdrop-blur-sm bg-opacity-20  relative mx-auto  mt-24 "
+            >
+              <img src={`${process.env.PUBLIC_URL}/m3.png`} alt="m" />
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <MobileSize />
+      )}
     </>
   );
 };
